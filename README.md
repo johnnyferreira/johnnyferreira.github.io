66138b4df644ca63e7f3bcd53f4fce4c9bdd0553
johnnyferreira <johnnyferreirabass@gmail.com>
2018-07-13 16:16:19 -0300

    MeuPortifolio

--- README.md
+++
@@ -1,1 +0,0 @@
-# johnnyferreira.github.io
---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Johnny Ferreira - Desenvolvedor Web</title>
+	<link href="https://fonts.googleapis.com/css?family=Anton|Architects+Daughter|Boogaloo|Carter+One|Caveat+Brush|Coda+Caption:800|Concert+One|Lalezar|Patrick+Hand|Russo+One|Signika+Negative|Wendy+One" rel="stylesheet"> 
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="public/assets/css/estilo.css">
+</head>
+<body>
+	<!-- Menu superior (fixo) -->
+	<nav class="menu-sup" style="border-radius: 0px">
+		<div class="col-lg-offset-2 col-lg-2 col-md-offset-2 col-md-2 col-sm-offset-2 col-sm-2 col-xs-3">Quem sou</div>
+		<div class="col-lg-2 col-md-2 col-sm-2 col-xs-3">Tecnologias</div>
+		<div class="col-lg-2 col-md-2 col-sm-2 col-xs-3">Portifolio</div>
+		<div class="col-lg-2 col-md-2 col-sm-2 col-xs-3">Contato</div>
+	</nav>
+
+	<!-- Banner do site -->
+	<div class="img-desfoque">
+		<header class="banner">
+			<div class="container">
+				<h1 class="titu-banner col-lg-12">Precisa de um Site?</h1>
+				<h1 class="sub-titu-banner col-lg-6">Compartilhe suas ideias com quem ama torna-las real!</h1>
+				
+			</div>
+		</header>
+	</div>
+	
+	<article class="artc1">
+		<h1>sobre mim</h1>
+		<img src="">
+		<div>Estudande de sistemas de informação. Etc...</div>
+	</article>
+	<article class="artc2">
+		<h1>Trabalhos realizados</h1>
+		<a href="#">link</a>
+		<a href="#">link</a>
+		<a href="#">link</a>
+		<a href="#">link</a>
+	</article>
+	<article class="artc3">
+		<h1>Ainda em duvida? Entre em contato ja</h1>
+		<form>
+			<input type="text" name="" placeholder="Digite seu nome">
+			<input type="email" name="" placeholder="Diigite seu email">
+			<input type="textarea" name="" placeholder="Sua mensagem">
+			<input type="submit" value="Enviar">
+		</form>
+	</article>
+	<footer>
+
+		<button class="btn btn-primary">Contratar Serviço</button>
+
+		<div>Facebook</div>
+		<div>Instagram</div>
+		<div>Whatsapp</div>
+
+		<div>Desenvolvido por Johnny Ferreira - 2018</div>
+	</footer>
+</body>
+</html>
